60efa1b1cab2e5f21572a0ea3d6f10d7506dc918
MateuszWyciszkiewicz <matiwycisz@gmail.com>
2022-10-17 13:14:45 +0200

    check if initial allocation was successful

--- StackInCpp/stack.cpp
+++ StackInCpp/stack.cpp
@@ -6,64 +6,73 @@ using namespace std;
 
 Stack::Stack()
 {
-    cout << "called constructor for stack\n" <<endl;
-	this->top = 0;
-	this->size = INITIAL_SIZE;
-	this->array = (int*)malloc(this->size * sizeof(int));
+    cout << "called constructor for stack\n" << endl;
+    this->top = 0;
+    this->size = INITIAL_SIZE;
+    this->array = (int*)malloc(this->size * sizeof(int));
+    if (this -> array == NULL) {
+        cout << "error during stack allocation" << endl;
+        abort();
+    }
 }
 
 Stack::~Stack()
 {
-	cout << "called destructor for stack\n" << endl;
-	free(this->array);
+    cout << "called destructor for stack\n" << endl;
+    free(this->array);
 }
 
 bool Stack::isFull()
 {
-	return (this->top == this->size - 1);
+    return (this->top == this->size - 1);
 }
 
 bool Stack::isEmpty()
 {
-	return (this->top == 0);
+    return (this->top == 0);
 }
 
 void Stack::resize()
 {
-	size_t newSize = (this->size) * 2;
-	int* newArray = (int*)realloc(this->array, newSize*sizeof(int));
-	if (newArray) {
-		this->array = newArray;
-        this -> size = newSize;
-	}
-	else {
-		cout << "error during stack reallocation" << endl;
-		abort();
-	}
+    size_t newSize = (this->size) * 2;
+    int* newArray = (int*)realloc(this->array, newSize * sizeof(int));
+    if (newArray) {
+        this->array = newArray;
+        this->size = newSize;
+    }
+    else {
+        cout << "error during stack reallocation" << endl;
+        abort();
+    }
 }
 
-void Stack::push(int number){
-	if(isFull()){
-		resize();
-		}
-	this -> array[this -> top] = number;
-	this -> top++;	
-	}
-	
-int Stack::pop(){
-	if(isEmpty()){
-		cout<<"poping from empty stack"<<endl;
-		abort();
-		} else {
-			this -> top--;
-			return this -> array[this -> top];
-			}
-	}
-
-size_t Stack::getSize(){
-    return this -> size;
+void Stack::push(int number)
+{
+    if (isFull()) {
+        resize();
     }
+    this->array[this->top] = number;
+    this->top++;
+}
 
-size_t Stack::getTop(){
-    return this -> top;
+int Stack::pop()
+{
+    if (isEmpty()) {
+        cout << "poping from empty stack" << endl;
+        abort();
     }
+    else {
+        this->top--;
+        return this->array[this->top];
+    }
+}
+
+size_t Stack::getSize()
+{
+    return this->size;
+}
+
+size_t Stack::getTop()
+{
+    return this->top;
+}
--- StackInCpp/testStack.cpp
+++ StackInCpp/testStack.cpp
@@ -11,83 +11,84 @@ void testOverlimitPush(Stack& stack);
 
 int main()
 {
-	Stack s1;
-	Stack s2;
-	testIsEmpty(s1);
-	testIsEmpty(s2);
-	testRegularPush(s1);
-	testRegularPush(s2);
-	testRegularPop(s1);
-	testRegularPop(s2);
-	testOverlimitPush(s1);
-	testOverlimitPush(s2);
-	testIsFull(s1);
-	testIsFull(s2);
-	//testPopOverlimit(s1);
-	return 0;
+    Stack s1;
+    Stack s2;
+    testIsEmpty(s1);
+    testIsEmpty(s2);
+    testRegularPush(s1);
+    testRegularPush(s2);
+    testRegularPop(s1);
+    testRegularPop(s2);
+    testOverlimitPush(s1);
+    testOverlimitPush(s2);
+    testIsFull(s1);
+    testIsFull(s2);
+    // testPopOverlimit(s1);
+    return 0;
 }
 
 void testIsEmpty(Stack& stack)
 {
-	cout << "Testing method isEmpty" << endl;
-	if (stack.isEmpty()) {
-		cout << "stack empty. Test successfull\n" << endl;
-	}
-	else {
-		cout << "Stack not empty, test failed\n" << endl;
-	}
+    cout << "Testing method isEmpty" << endl;
+    if (stack.isEmpty()) {
+        cout << "stack empty. Test successfull\n" << endl;
+    }
+    else {
+        cout << "Stack not empty, test failed\n" << endl;
+    }
 }
 
 void testRegularPush(Stack& stack)
 {
-	cout << "Testing method push" << endl;
-	int num = 5;
-	stack.push(num);
-	cout << "Push function test successful\n\n" << endl;
+    cout << "Testing method push" << endl;
+    int num = 5;
+    stack.push(num);
+    cout << "Push function test successful\n\n" << endl;
 }
 
 void testRegularPop(Stack& stack)
 {
-	cout << "Testing method pop" << endl;
-	int num = 5;
-	int result = stack.pop();
-	if (result == num) {
-		cout << "Pop function test successful\n\n" << endl;
-	}
-	else {
-		cout << "Pop function test failed\n\n" << endl;
-	}
+    cout << "Testing method pop" << endl;
+    int num = 5;
+    int result = stack.pop();
+    if (result == num) {
+        cout << "Pop function test successful\n\n" << endl;
+    }
+    else {
+        cout << "Pop function test failed\n\n" << endl;
+    }
 }
 
 void testIsFull(Stack& stack)
 {
-	cout << "Testing isFull function" << endl;
-	for (int i = 0; stack.getTop() < stack.getSize()-1; i++) {
-		stack.push(i);
-	}
-	if (stack.isFull()) {
-		cout << "Stack full, test successful\n\n";
-	}
-	else {
-		cout << "Stack not full, test failed\n\n";
-	}
+    cout << "Testing isFull function" << endl;
+    for (int i = 0; stack.getTop() < stack.getSize() - 1; i++) {
+        stack.push(i);
+    }
+    if (stack.isFull()) {
+        cout << "Stack full, test successful\n\n";
+    }
+    else {
+        cout << "Stack not full, test failed\n\n";
+    }
 }
 
 void testOverlimitPush(Stack& stack)
 {
-	cout << "Testing push function over stack limit" << endl;
-	for (int i = 0; i < INITIAL_SIZE * 2.; i++) {
-		stack.push(i);
-	}
-	cout << "Overlimit push test successful, init size:" << INITIAL_SIZE<<" current size: "<<stack.getSize() <<"\n\n";
+    cout << "Testing push function over stack limit" << endl;
+    for (int i = 0; i < INITIAL_SIZE * 2.; i++) {
+        stack.push(i);
+    }
+    cout << "Overlimit push test successful, init size:" << INITIAL_SIZE
+         << " current size: " << stack.getSize() << "\n\n";
 }
 
 void testPopOverlimit(Stack& stack)
 {
-	cout << "Testing overlimit pop. Program should receive error after this "
-			"function is invoked:"
-		 << endl;
-	for (size_t i = 0; i < stack.getSize(); i++) {
-		stack.pop();
-	}
+    cout << "Testing overlimit pop. Program should receive error after this "
+            "function is invoked:"
+         << endl;
+    for (size_t i = 0; i < stack.getSize(); i++) {
+        stack.pop();
+    }
 }
