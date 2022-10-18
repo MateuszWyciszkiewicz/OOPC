73b4bcc5c976ac6df27964aa3c121831c002d8cc
MateuszWyciszkiewicz <matiwycisz@gmail.com>
2022-10-18 15:59:04 +0200

    unit tests

--- ExtendedStack/testStack.cpp
+++ ExtendedStack/testStack.cpp
@@ -10,16 +10,21 @@ void testPopOverlimit(Stack& stack);
 void testOverlimitPush(Stack& stack);
 void generalTests(Stack& s1, Stack& s2);
 
+void printResults(Stack& modelStack, Stack& moddedStack);
+void populateStacks(Stack& larger, Stack& smaller);
+void copyTest();
+void assignLargerToSmaller();
+void assignSmallerToLarger();
+
 
 int main()
 {
-	Stack s1;
-	Stack s2;
-    generalTests(s1, s2);
-    Stack s3;
-    s2 = s2;
-    s2.printStack();
-    s3.printStack();
+	//Stack s1;
+	//Stack s2;
+    //generalTests(s1, s2);
+    copyTest();
+    assignLargerToSmaller();
+    assignSmallerToLarger();
 	return 0;
 }
 
@@ -35,8 +40,60 @@ void generalTests(Stack& s1, Stack& s2)
 	testOverlimitPush(s2);
 	testIsFull(s1);
 	testIsFull(s2);
-	// testPopOverlimit(s1);
+	testPopOverlimit(s1);
 }
+
+void copyTest(){
+    cout << "Copying stack test"<<endl;
+    Stack s1;
+    for(int i =0; i < INITIAL_SIZE - 1; i++){
+        s1.push(i);
+        }
+    
+    Stack s2(s1);
+   printResults(s1, s2);
+    }
+
+void populateStacks(Stack& larger, Stack& smaller){
+    for(int i =0; i < INITIAL_SIZE * 2; i++){
+        larger.push(i);
+        }
+    for(int i =0; i < INITIAL_SIZE - 1; i++){
+        smaller.push(i);
+        }
+    }
+
+void printResults(Stack& modelStack, Stack& moddedStack){
+    cout << "Model stack: "<< endl;
+    modelStack.printStack();
+    cout << "Modified stack: "<< endl;
+    moddedStack.printStack();
+    cout << endl;
+    }
+
+void assignLargerToSmaller(){
+    cout << "Assigning larger to smaller test"<<endl;
+    Stack larger, smaller;
+    cout << "before:" << endl;
+    populateStacks(larger, smaller);
+    printResults(smaller, larger);
+    larger = smaller;
+    cout << "after:" << endl;
+    printResults(smaller, larger);
+    
+    }
+
+void assignSmallerToLarger(){
+    cout << "Assigning larger to smaller test"<<endl;
+    Stack larger, smaller;
+    cout << "before:" << endl;
+    populateStacks(larger, smaller);
+    printResults(smaller, larger);
+    smaller = larger;
+    cout << "after:" << endl;
+    printResults(larger, smaller);
+    
+    }
 void testIsEmpty(Stack& stack)
 {
 	cout << "Testing method isEmpty" << endl;
