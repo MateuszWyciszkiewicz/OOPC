fcdfc1027f6b8b161216fe72024040e8779a04be
MateuszWyciszkiewicz <matiwycisz@gmail.com>
2022-12-26 17:52:33 +0100

    special cases tests

--- MapTemplate/Map.h
+++ MapTemplate/Map.h
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+class KeyDoesNotExistException {};
+class KeyAlreadyExistsException {};
 template <class Key, class Value> class Map {
   private:
 	size_t size;
@@ -12,6 +14,7 @@ template <class Key, class Value> class Map {
 	struct entry {
 		Key key;
 		Value value;
+        entry(){};
 		entry(Key inKey, Value inValue)
 		{
 			key = inKey;
@@ -19,7 +22,7 @@ template <class Key, class Value> class Map {
 		}
 	};
 
-	entry* array;
+	entry* data;
 
 	bool isFull()
 	{
@@ -36,21 +39,86 @@ template <class Key, class Value> class Map {
 	}
 
   public:
-    Map() {
-        this -> data = new entry[2];
-        this -> size = 2;
-        this -> top = 0;
-        };
-        
-    Map(consst Map& m){
-        this -> size = m.size;
-        this -> top = m.top;
-        delete this -> data;
-        this -> data = new entry[this -> size];
-        copy(m.data, m.data + m.top + 1, this -> data);
-        };
-        
-        ~Map() {
-            delete[] this -> data;
+	Map()
+	{
+		this->data = new entry[2];
+		this->size = 2;
+		this->top = 0;
+	};
+
+	Map(const Map& m)
+	{
+		this->size = m.size;
+		this->top = m.top;
+		this->data = new entry[this->size];
+		copy(m.data, m.data + m.top + 1, this->data);
+	};
+
+	~Map()
+	{
+		delete[] this->data;
+	}
+
+	Map& operator=(const Map& m)
+	{
+		if (this == &m) {
+			return *this;
+		}
+		if (m.top + 1 > this->size) {
+			this->size = m.size;
+			delete[] this->data;
+			this->data = new entry[this->size];
+		}
+		this->top = m.top;
+		copy(m.data, m.data + m.top + 1, this->data);
+		return *this;
+	}
+
+	Value* find(Key key)
+	{
+		for (int i = 0; i < (int)this->top; i++) {
+			if (this->data[i].key == key) {
+				return &this->data[i].value;
+			}
+		}
+		throw KeyDoesNotExistException();
+	}
+
+	void add(Key key, Value val)
+	{
+		if (isFull()) {
+			resize();
+		}
+		if (top == 0) {
+			this->data[0].key = key;
+			this->data[0].value = val;
+		}
+		else {
+			for (int i = 0; i <= (int)this->top; i++) {
+				if (this->data[i].key > key || i == (int)top) {
+					copy(this->data + i, this->data + this->top + 1,
+						 this->data + i + 1);
+					this->data[i].key = key;
+					this->data[i].value = val;
+					break;
+				}
+				else if (this->data[i].key == key) {
+					throw KeyAlreadyExistsException();
+				}
+			}
+		}
+		this->top++;
+	}
+    
+    friend ostream& operator << (ostream& out, const Map& m){
+        for(int i = 0; i < (int) m.top; i++){
+            out << i+1;
+            out << ". ";
+            out << m.data[i].key;
+            out << " ";
+            out << m.data[i].value;
+            out << endl;
             }
+            return out;
+        }
 };
--- MapTemplate/TestMap.cpp
+++ MapTemplate/TestMap.cpp
@@ -1,5 +1,86 @@
-#include "Map.h"
+#include "Employee.h" // Defines class Employee
+#include "Map.h"	  // Defines template Map<Key, Value>
+#include <iostream>
 
-int main(){
-    return 0;
-    }
+typedef unsigned int ID;			// Identification number of employee
+typedef Map<ID, Employee> Database; // Database of employees
+
+using namespace std;
+
+void addEmployees(Database& database);
+void modifyEmployees(Database& database);
+void specialCases(Database& database);
+void keyNotInBase(Database& database);
+void keyMultipletimes(Database& database);
+
+int main()
+{
+	Database database;
+	addEmployees(database);
+
+	Database newDatabase = database; // Make a copy of database
+
+	newDatabase.add(830505432, Employee("Ewa Nowak", "charwoman",
+										43)); // Add fourth employee
+	modifyEmployees(newDatabase);
+
+	cout << "Original database:" << endl << database << endl;
+	cout << "Modified database:" << endl << newDatabase << endl;
+
+	database = newDatabase; // Update original database
+
+	cout << "Database after the assignment:" << endl << database << endl;
+	specialCases(database);
+};
+
+void specialCases(Database& database)
+{
+	cout << "special cases tests\n" << endl;
+	keyNotInBase(database);
+	keyMultipletimes(database);
+}
+
+void addEmployees(Database& database)
+{
+	database.add(761028073,
+				 Employee("Jan Kowalski", "salesman",
+						  28)); // Add first employee: name: Jan Kowalski,
+								// position: salseman, age: 28,
+	database.add(510212881, Employee("Adam Nowak", "storekeeper",
+									 54)); // Add second employee
+	database.add(730505129, Employee("Anna Zaradna", "secretary",
+									 32)); // Add third employee
+}
+
+void modifyEmployees(Database& database)
+{
+	Employee* employeePtr;
+
+	employeePtr = database.find(510212881); // Find employee using its ID
+	employeePtr->position = "salesman";		// Modify the position of employee
+
+	employeePtr = database.find(761028073); // Find employee using its ID
+	employeePtr->age = 29;					// Modify the age of employee
+}
+
+void keyNotInBase(Database& database)
+{
+	cout << "key not in the database test:" << endl;
+	try {
+		database.find(122222);
+	}
+	catch (KeyDoesNotExistException e) {
+		cout << "key not in database, exception caught\n" << endl;
+	}
+}
+
+void keyMultipletimes(Database& database)
+{
+	cout << "key alredy in database test:" << endl;
+	try {
+		database.add(761028073, Employee("Jan Nowak", "Manager", 24));
+	}
+	catch (KeyAlreadyExistsException e) {
+		cout << "key already in database, exception caught\n" << endl;
+	}
+}
